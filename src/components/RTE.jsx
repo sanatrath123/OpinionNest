5b18fe011a116e039ba1b0eac7855a3fbf2b5dd1
sanatrath123 <rathsanantakumar@gmail.com>
2023-12-27 03:48:58 +0530

    fix rich editor change

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,56 +1,41 @@
-import React from 'react'
-import { Editor } from '@tinymce/tinymce-react'
-import { Controller } from 'react-hook-form'
+import { Editor } from "@tinymce/tinymce-react"
+import { Controller } from "react-hook-form"
 
-function RTE({name , control , label ,defaultvalue=""}) {
-
+function RTE({name ,label, control , defaultValue=""}) {
   return (
-    <div className='w-full'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
-
-    <Controller
-    name={name || "content"}
+    <div className="w-full">
+    {label && <label className="inline-block mb-2 p-1 ">{label}</label>}
+    <Controller>
+ name={name || "content"}
     control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
-    )}
-    />
+    render={({ field: { onChange } }) => (
+(    <Editor
+          initialValue= {defaultValue}
+         init={{
+           height: 500,
+           menubar: true,
+           plugins: [
+             'advlist autolink lists link image charmap print preview anchor',
+             'searchreplace visualblocks code fullscreen',
+             'insertdatetime media table paste code help wordcount'
+           ],
+           toolbar: ['undo redo | formatselect | ' +
+           'bold italic backcolor | alignleft aligncenter ' +
+           'alignright alignjustify | bullist numlist outdent indent | ' +
+           'removeformat | help'],
+           content_style: ['body { font-family:Helvetica,Arial,sans-serif; font-size:14px }']
+         }}
+         onEditorChange={onChange}
+       />
+)
+       )}
+    
 
-     </div>
+    
+    </Controller>  
+    </div>
   )
-}
   
+}
+
 export default RTE
